7e309eadbc0b782d61a0819b593d343ab93b8263
Crowdbotics <team@crowdbotics.com>
2020-08-14 08:23:58 +0000

    Update models

--- backend/menelik_ii_19552/wsgi.py
+++ backend/menelik_ii_19552/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menelik_ii_19552.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "menelik_ii_19552.settings")
 
 application = get_wsgi_application()
